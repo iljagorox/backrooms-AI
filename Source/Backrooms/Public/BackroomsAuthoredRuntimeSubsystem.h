5577b03980060e98e0df7c3f8fc5c90bca874ec1
iljagorox <87163236+iljagorox@users.noreply.github.com>
2026-09-16 01:00:25 +0300

    Add authored chunk runtime bridge

---
+++ Source/Backrooms/Public/BackroomsAuthoredRuntimeSubsystem.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "BackroomsAuthoredRuntimeSubsystem.generated.h"
+
+class ABackroomsChunkActor;
+class UBackroomsAuthoredChunk;
+class UBackroomsAuthoredChunkLibrary;
+
+UCLASS()
+class BACKROOMS_API UBackroomsAuthoredRuntimeSubsystem : public UTickableWorldSubsystem
+{
+    GENERATED_BODY()
+
+public:
+    virtual void Tick(float DeltaTime) override;
+    virtual TStatId GetStatId() const override;
+    virtual bool IsTickable() const override { return true; }
+    virtual bool ShouldCreateSubsystem(UObject* Outer) const override;
+
+private:
+    void ProcessChunk(ABackroomsChunkActor* Chunk);
+    UBackroomsAuthoredChunkLibrary* LoadLibrary();
+    bool BuildAuthoredChunk(ABackroomsChunkActor* Chunk, UBackroomsAuthoredChunk* Authored);
+    void FillSockets(ABackroomsChunkActor* Chunk, UBackroomsAuthoredChunk* Authored);
+    void ClearLegacyOwnedActors(ABackroomsChunkActor* Chunk) const;
+
+    UPROPERTY()
+    TMap<TObjectPtr<ABackroomsChunkActor>, FName> ProcessedChunks;
+
+    UPROPERTY()
+    TObjectPtr<UBackroomsAuthoredChunkLibrary> CachedLibrary;
+};
